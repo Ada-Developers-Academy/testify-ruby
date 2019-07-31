68eaa1e1b916b8219cf3301ac590fb2945667346
Kaida Masaki <kaida@adadevelopersacademy.org>
2019-07-31 13:59:12 -0700

    Make codeParser return the line and Range

--- src/parser/codeParser.ts
+++ src/parser/codeParser.ts
@@ -1,4 +1,5 @@
-// TODO: "describe" support?
+import { Position, Range } from "vscode";
+// TODO: Add support for `describe` blocks?
 
 const singleStringPattern = "(?:'(?:(?:[^']|\\\\')*[^\\'])')";
 const doubleStringPattern = '(?:"(?:(?:[^"]|\\\\")*[^\\"])")';
@@ -7,14 +8,45 @@ const testNamePattern = `(?:(?:[(]\\s*${stringPattern}\\s*[)])|${stringPattern})
 
 const itPattern = new RegExp(`\\b(it\\s*${testNamePattern})`, "g");
 
-function codeParser(sourceCode) {
-  return [...sourceCode.matchAll(itPattern)].map(match => ({
-    loc: {
-      end: match.index + match[0].length,
-      start: match.index
-    },
-    testName: match[0]
-  }));
+interface ITest {
+  loc: Range;
+  testName: string;
+}
+
+function findPosition(lengths: number[], index: number): Position {
+  let line = 0;
+
+  for (const len of lengths) {
+    if (index < len) {
+      return new Position(line, index);
+    } else {
+      line += 1;
+      index -= len;
+    }
+  }
+
+  throw new Error("Position does not exist in file!");
+}
+
+function codeParser(sourceCode: string): ITest[] {
+  const lengths = sourceCode.split(/\r?\n/).map(line => line.length + 1);
+
+  const matches: ITest[] = [];
+  let match = itPattern.exec(sourceCode);
+
+  while (match !== null) {
+    matches.push({
+      loc: new Range(
+        findPosition(lengths, match.index),
+        findPosition(lengths, match.index + match[0].length)
+      ),
+      testName: match[0]
+    });
+
+    match = itPattern.exec(sourceCode);
+  }
+
+  return matches;
 }
 
 export { codeParser };

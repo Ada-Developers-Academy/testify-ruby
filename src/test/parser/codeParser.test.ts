7535d76ad93db546064565976640111196726c8b
Kaida Masaki <kaida@adadevelopersacademy.org>
2019-08-01 10:13:14 -0700

    Update tests for new code parser

--- src/test/parser/codeParser.test.ts
+++ src/test/parser/codeParser.test.ts
@@ -1,72 +1,46 @@
-//
-// Note: This example test is leveraging the Mocha test framework.
-// Please refer to their documentation on https://mochajs.org/ for help.
-//
-
 // The module 'assert' provides assertion methods from node
 import * as assert from "assert";
 
 import { codeParser } from "../../parser/codeParser";
 
-// Defines a Mocha test suite to group tests of similar kind together
 suite("codeParser Tests", () => {
-  // Defines a Mocha unit test
-  test("Valid Token", () => {
-    const code = `
-            describe('Fake test', () => {});
-        `;
-    assert.equal(1, codeParser(code).length);
-  });
-
-  test("Invalid Tokens", () => {
-    const code = `
-            var test = 'Fluo';
-            let src = {test: true, type: 'BANK'};
-            if (!src.test && src.type === 'BANK') {
-                let firstName = 'test';
-            }
-        `;
+  test("Incomplete test", () => {
+    const code = `it(`;
     assert.equal(0, codeParser(code).length);
   });
 
-  test("Jsx syntax", () => {
-    const code = `
-        describe("JsonFormTextField", () => {
+  test("Single quotes, parens", () => {
+    const code = `it('Fake test')`;
 
-            test("Test render", () => {
-                const config: ITextFieldConfig = {
-                    fieldType: "text",
-                    name: "Owner",
-                };
-                const wrapper = mount(<FormWrapper>
-                    <JsonFormTextField
-                        config={config}
-                        value={null} />
-                </FormWrapper>);
-                const ownerInput = wrapper.find("input[type='text'][id='Owner']");
-                expect(ownerInput.length).toBe(1);
-                ownerInput.simulate("change");
-            });
-        });
-        `;
-    assert.equal(2, codeParser(code).length);
+    assert.equal(1, codeParser(code).length);
   });
 
-  test("Rest Spread syntax", () => {
+  test("Single quotes, no parens", () => {
+    const code = `it 'Fake test'`;
+    assert.equal(1, codeParser(code).length);
+  });
+
+  test("Double quotes, parens", () => {
+    const code = `it("Fake test")`;
+
+    assert.equal(1, codeParser(code).length);
+  });
+
+  test("Double quotes, no parens", () => {
+    const code = `it "Fake test"`;
+    assert.equal(1, codeParser(code).length);
+  });
+
+  test("Multiple tests", () => {
     const code = `
-        describe("TestWithRestSpread", () => {
-          it('just works!', () => {
-            const obj = {
-              name: 'Testify',
-              catchphrase: 'Witness Me!',
-            };
-
-            const newObject = { ...obj };
-
-            expect(newObject.catchphrase).toBe('Witness Me!');
-          });
-        });
-      `;
-    assert.equal(2, codeParser(code).length);
+it "Fake test"
+Not a test
+it("Fake test 2")
+Not a test
+it 'Fake test 3'
+Not a test
+it ('Fake test 4')
+`;
+    assert.equal(4, codeParser(code).length);
   });
 });
